5aa7ed450895c2287ebe2a295fc623ec60a80b5e
hubkra <goodguy.kras@gmail.com>
2021-11-20 20:11:53 +0100

    lab3zad

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+using System;
+namespace Lab3_test3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/FormModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab3_test3.Models
+{
+    public class FormModel
+    { [Display(Name = "Imię")]
+        [Required]
+        public string Name { get; set; }
+        [Display(Name = "Liczba powtórzeń")]
+        public int numRepeat { get; set; }
+    }
+}
